4ef4f7902aff960f564ae33de16917ded5022b58
PadraicHeberlein <padraic.heb@gmail.com>
2025-01-12 11:05:31 -0500

    Add SqMatrix class.

---
+++ Linear/SqMatrix.cs
@@ -0,0 +1,42 @@
+namespace Algebra.Linear {
+    public class SqMatrix : Matrix{
+        private int n;
+        public SqMatrix() : base() { }
+
+        public SqMatrix(int size) : base(size, size) { n = size; }
+
+
+        public SqMatrix Minor(int exRow, int exCol) {
+            int minorRow = 0, minorCol = 0;
+            SqMatrix minor = new SqMatrix(n - 1);
+
+            for (int row = 0; row < n; row++) {
+                if (row == exRow) continue;
+                else {
+                    for (int col = 0; col < n; col++) {
+                        if (col == exCol) continue;
+                        else {
+                            minor.Set(
+                                minorRow, 
+                                minorCol, 
+                                minor.Get(row, col)
+                            );
+                            minorCol++;
+                        }
+                    }
+                    minorRow++;
+                }
+                
+            }
+
+            return minor;
+        }
+        public double Trace() {
+            double trace = 0;
+
+            for (int d = 0; d < n; d++) trace += elements[d, d];
+
+            return trace;
+        }
+    }
+}
